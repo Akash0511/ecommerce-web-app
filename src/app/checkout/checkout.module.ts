3424dfd72d58acf400707930ef4bbd3bf4ccb393
Akash Bhardwaj <bhardwajakash78@gmail.com>
2021-04-23 00:04:05 +0530

    project structure setup

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CheckoutRoutingModule } from './checkout-routing.module';
+import { CartComponent } from './components/cart/cart.component';
+import { PlaceOrderComponent } from './components/place-order/place-order.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CartComponent,
+    PlaceOrderComponent
+  ],
   imports: [
     CommonModule,
     CheckoutRoutingModule
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProductDetailPageComponent } from './components/product-detail-page/product-detail-page.component';
+import { SearchProductComponent } from './components/search-product/search-product.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    MainPageComponent,
+    ProductDetailPageComponent,
+    SearchProductComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
